3785d1738fddb3678ceb49a1e1593a07dfd14891
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2019-08-25 22:58:18 +0300

    refactor: Kill PseudoMap class

--- src/_core/defs.js
+++ src/_core/defs.js
@@ -1,25 +1,2 @@
-import assign from '../_helpers/assign';
-
-function PseudoMap() {}
-
-// PseudoMap simulates WeakMap behavior with O(1) search complexity
-// it's needed to support @IE9 and @IE10
-/* istanbul ignore next */
-assign(PseudoMap.prototype, {
-    get(obj) {
-        return obj.defi;
-    },
-    set(obj, data) {
-        Object.defineProperty(obj, 'defi', {
-            value: data,
-            enumerable: false,
-            writable: false,
-            configurable: false
-        });
-    },
-    has(obj) {
-        return 'defi' in obj;
-    }
-});
-
-export default typeof WeakMap === 'undefined' ? new PseudoMap() : new WeakMap();
+// object definitions
+export default new WeakMap();
